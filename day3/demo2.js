ebef7df12eebf4699aa768d4a77754c886207b5d
jaykaneriya6143 <84986966+jaykaneriya6143@users.noreply.github.com>
2021-05-30 19:58:07 +0530

    Add files via upload

---
+++ day3/demo2.js
@@ -0,0 +1,3 @@
+setInterval(function(){
+    console.log("hello i am jay");
+},2000);
---
+++ day3/task1.js
@@ -0,0 +1,10 @@
+function jk(){
+    console.log('hello world');
+}
+
+function sayname(name){
+    console.log('hello' + ''+ name);
+}
+
+setTimeout(jk,1000);
+sayname('jay');
